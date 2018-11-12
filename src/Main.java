2313f2f04bf33f86fd55148ab68b83614fc5ec55
ihuanglele <huanglele@yousuowei.cn>
2018-11-12 11:48:06 +0800

    优化代码

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
 import com.ihuanglele.book.Runner;
 import com.ihuanglele.book.store.IStore;
 import com.ihuanglele.book.store.SqliteStore;
-import com.ihuanglele.book.strategy.AbstractSite;
-import com.ihuanglele.book.strategy.QisuuLa;
-import com.ihuanglele.book.util.Tool;
 
 class Main {
 
     public static void main(String[] args) {
-//        IStore store = new SqliteStore();
-//        Runner runner = new Runner();
-//        runner.setSiteName("QisuuLa");
-//        runner.setStore(store);
-//        runner.setStart("1");
-//        runner.run();
-
-        Tool.save("nihaoya 1","text");
-        Tool.save("nihaoya 2","text");
-        Tool.save("nihaoya 3","text");
-        Tool.save("nihaoya 4","text");
-        Tool.closeFileWriter();
+        IStore store = new SqliteStore();
+        Runner runner = new Runner();
+        runner.setSiteName("QisuuLa");
+        runner.setStore(store);
+        runner.setStart("1");
+        runner.run();
     }
 
 }
--- src/com/ihuanglele/book/util/Tool.java
+++ src/com/ihuanglele/book/util/Tool.java
@@ -1,7 +1,5 @@
 package com.ihuanglele.book.util;
 
-import com.ihuanglele.book.page.Book;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
